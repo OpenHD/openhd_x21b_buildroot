09fd009cf93adb0a3bfdfbe7f6f77f9c668a7035
OpenHD-Automation <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-27 13:47:16 +0000

    package/OpenHD: update to 75052b0e757169c0a26b5d1b8fbf09bb85f2a083

--- package/OpenHD/openhd.mk
+++ package/OpenHD/openhd.mk
@@ -29,7 +29,7 @@ OPENHD_GIT_SUBMODULES = YES
 
 # Always resolve to the current HEAD of the openhd-3.0 branch
 # Note: this is not reproducible and is not the recommended Buildroot approach.
-OPENHD_VERSION = d4373fda4616053746931e965399738c61470692
+OPENHD_VERSION = 75052b0e757169c0a26b5d1b8fbf09bb85f2a083
 
 # Enable Git submodules if the project requires them
 OPENHD_GIT_SUBMODULES = YES
