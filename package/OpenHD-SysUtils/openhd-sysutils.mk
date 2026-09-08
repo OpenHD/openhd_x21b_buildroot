2eb20484dd8dd2178ac0353569c471bca836ce25
OpenHD-Automation <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-08 20:35:14 +0000

    package/OpenHD-SysUtils: update to 0e89ff19506634dbdfcedc71befa2d9545eee5d3

--- package/OpenHD-SysUtils/openhd-sysutils.mk
+++ package/OpenHD-SysUtils/openhd-sysutils.mk
@@ -28,7 +28,7 @@ OPENHD_SYSUTILS_SITE_METHOD = git
 OPENHD_SYSUTILS_GIT_SUBMODULES = YES
 
 # Set the version to the latest commit of the default branch
-OPENHD_SYSUTILS_VERSION = 4820f9d8658c68e4a769e4a3b5e0e584aa6c3777
+OPENHD_SYSUTILS_VERSION = 0e89ff19506634dbdfcedc71befa2d9545eee5d3
 
 # Enable Git submodules if the project requires them
 OPENHD_SYSUTILS_GIT_SUBMODULES = YES
